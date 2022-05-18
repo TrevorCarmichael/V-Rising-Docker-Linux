213a7b19d10ed8f63336d4049c54019373e2bdd0
Googlrr <Googlrr@gmail.com>
2022-05-18 00:46:37 -0400

    Update run.sh

--- root/scripts/run.sh
+++ root/scripts/run.sh
@@ -4,10 +4,6 @@ if [ ! -e "./steaminstalled.txt" ]; then
     wincfg
     wine steamcmd.exe +force_install_dir "C:\VRisingServer" +login anonymous +app_update 1829350 validate +quit
     touch ./steaminstalled.txt
-    mv /scripts/ServerGameSettings.json /root/.wine/drive_c/users/root/AppData/LocalLow/'Stunlock Studios'/VRisingServer/Settings/
-    mv /scripts/ServerHostSettings.json /root/.wine/drive_c/users/root/AppData/LocalLow/'Stunlock Studios'/VRisingServer/Settings/
-    mv /scripts/adminlist.txt /root/.wine/drive_c/users/root/AppData/LocalLow/'Stunlock Studios'/VRisingServer/Settings/
-    mv /scripts/banlist.txt /root/.wine/drive_c/users/root/AppData/LocalLow/'Stunlock Studios'/VRisingServer/Settings/
 fi
 
 rm -r /tmp/.X0-lock
