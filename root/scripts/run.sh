e2adf54efe9ecdc233209b05751fd0f04c828e16
Googlrr <Googlrr@gmail.com>
2022-05-18 00:34:05 -0400

    mapped settings as a volume
    and other stuff

--- root/scripts/run.sh
+++ root/scripts/run.sh
@@ -6,8 +6,11 @@ if [ ! -e "./steaminstalled.txt" ]; then
     touch ./steaminstalled.txt
     mv /scripts/ServerGameSettings.json /root/.wine/drive_c/users/root/AppData/LocalLow/'Stunlock Studios'/VRisingServer/Settings/
     mv /scripts/ServerHostSettings.json /root/.wine/drive_c/users/root/AppData/LocalLow/'Stunlock Studios'/VRisingServer/Settings/
+    mv /scripts/adminlist.txt /root/.wine/drive_c/users/root/AppData/LocalLow/'Stunlock Studios'/VRisingServer/Settings/
+    mv /scripts/banlist.txt /root/.wine/drive_c/users/root/AppData/LocalLow/'Stunlock Studios'/VRisingServer/Settings/
 fi
 
+rm -r /tmp/.X0-lock
 cd /root/.wine/drive_c/VRisingServer/
 Xvfb :0 -screen 0 1024x768x16 & \
 DISPLAY=:0.0 wine VRisingServer.exe
